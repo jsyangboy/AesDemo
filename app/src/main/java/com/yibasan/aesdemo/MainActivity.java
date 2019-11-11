84be16ea656801b5716aa61124be75e44cba2211
yqy <yangqingyuan@lizhi.fm>
2019-11-11 18:20:06 +0800

    base

--- app/src/main/java/com/yibasan/aesdemo/MainActivity.java
+++ app/src/main/java/com/yibasan/aesdemo/MainActivity.java
@@ -1,10 +1,11 @@
 package com.yibasan.aesdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,21 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
 
                 try {
-                    //String key = AesUtil.generateKey(128);
-                    String key = "BA7372A153EBF8FF921F1ED4DB76099B";
                     String test = "test";
-                    System.out.println("key=" + key);
-                    System.out.println("encode=" + Base64.encodeToString(AesUtil.encode(key, test), Base64.DEFAULT));
-                    System.out.println("decode=" + new String(AesUtil.decode(key, Base64.decode("2g8pNUVF+rpsnBrGKuaNug==", Base64.DEFAULT))));
-
-                    System.out.println("encode2=" + AesUtil.encode2(key, test));
-                    System.out.println("decode2=" + AesUtil.decode2(key, "2g8pNUVF+rpsnBrGKuaNug=="));
-
-
+                    String string = AESCipher.aesEncryptString(test, "BA7372A153EBF8FF");//BA7372A153EBF8FF//16BytesLengthKey
+                    System.out.println(string);
+                    System.out.println(AESCipher.aesDecryptString(string, "BA7372A153EBF8FF"));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
